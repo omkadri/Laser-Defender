421d44f097c70aa62e132cf52db42049eee527a8
omkadri <omkadri@hotmail.com>
2019-10-27 18:38:09 -0500

    added enemy waypoints to scene. created enemy pathing script, containg a list that the waypoints can attach to

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints; // The type is Transform because that is the only data type one the waypoint gameObject we are interested in
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
