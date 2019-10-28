6bcdb6d00d8af2f70227c2a9554c0ddc62adf89e
omkadri <omkadri@hotmail.com>
2019-10-27 19:16:16 -0500

    made enemy move along path
    made enemy move along path using waypointIndex and Vector2.MoveTowards

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,16 +5,41 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] List<Transform> waypoints; // The type is Transform because that is the only data type one the waypoint gameObject we are interested in
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;//
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position; // this lines spawns the enemy object at the first waypoint on the path(index 0)
+
     }
 
     // Update is called once per frame
     void Update()
+    {   
+        Move();
+
+    }
+
+    private void Move()
     {
-        
+        if (waypointIndex <= waypoints.Count - 1) // we use .Count because waypoints is a List. If it was an array, we would use .Length
+        {
+            var targetPostion = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPostion, movementThisFrame);
+
+            if (transform.position == targetPostion)
+            {
+                waypointIndex++;
+            }
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
