5601296c9a077f1bc7082162ec435c51f0c9c486
omkadri <omkadri@hotmail.com>
2019-11-06 00:16:30 -0600

    Enemies can now take damage,
    I created a DamageDealer script that contains data on the value of damage an object can deal. This is now attached to the laser.
    
    I also created an enemy script that contains health information and code for collision. When colliding with an object that has a DamageDealer script(in this case, the laser), the enemy will get the damage value from object and subtract it from the enemy health variable.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 100; //don't you have to put an f after a float???
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>(); //retrieves the DamageDealer info from the object being collided with
+        health -= damageDealer.GetDamage();//retrieves damage data from DamageDealer script and subtracts it from enemy health
+    }
+}
