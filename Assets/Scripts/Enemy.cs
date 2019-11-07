75bc12d80010539724f8b828cc7e7e3c987e71fe
omkadri <omkadri@hotmail.com>
2019-11-06 21:19:51 -0600

    Enemies can now shoot lasers.
    Created a shotCounter variable. It is initialized randomly. When it reaches zero, the enemy prefab it is attached to fires a laser(using the same code as player shooting) and the shotCounter resets.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,22 +5,56 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int health = 100; //don't you have to put an f after a float???
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float enemyProjectileSpeed = 10f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <=0)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject EnemyLaser = Instantiate
+            (enemyLaserPrefab, transform.position, Quaternion.identity) //Quaternion.identity means just use the rotation that you have
+                as GameObject; // what does this mean?????
+        EnemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyProjectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>(); //retrieves the DamageDealer info from the object being collided with
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
         health -= damageDealer.GetDamage();//retrieves damage data from DamageDealer script and subtracts it from enemy health
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
