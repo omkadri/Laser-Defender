f5d2dc82cc5c7b13aa800af4315244fa97cae810
omkadri <omkadri@hotmail.com>
2020-01-13 18:10:36 -0500

    Started importing assets for another enemy type named "Big Boy"

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float speedOfSpin = 1f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
+    }
+}
